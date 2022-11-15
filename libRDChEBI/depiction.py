265fc40e50c74ca158488b57a7db0a47e49d98dd
Eloy Felix <eloyfelix@gmail.com>
2022-11-15 15:56:05 +0000

    rdkit2022.09.02 fixes

--- libRDChEBI/depiction.py
+++ libRDChEBI/depiction.py
@@ -6,15 +6,6 @@ import io
 
 def depict(molfile, height=400, width=400):
     mol = parse_molblock(molfile)
-
-    # hack to get back R's without 'M RGP' entry in molfile
-    f = io.StringIO(molfile)
-    ctf = ctfile.load(f)
-    for at, rd_at in zip(ctf.atoms, mol.GetAtoms()):
-        if at.atom_symbol[0] == "R" and rd_at.GetSymbol() == "*":
-            rd_at.SetProp("atomLabel", "R")
-    # hack
-
     d = rdMolDraw2D.MolDraw2DSVG(height, width)
     draw_options = d.drawOptions()
     draw_options.minFontSize = 8
