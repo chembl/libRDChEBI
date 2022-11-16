7df99026a885794189ffd41471888da5bb9b7c7d
Eloy Felix <eloyfelix@gmail.com>
2022-11-16 16:36:34 +0000

    rdkit2022.09.02 fixes

--- libRDChEBI/__init__.py
+++ libRDChEBI/__init__.py
@@ -1,2 +1,7 @@
 from .depiction import *
 from .descriptors import *
+import rdkit
+
+rdkversion = rdkit.__version__.split(".")
+if rdkversion < ["2022", "09", "2"]:
+    raise ValueError("need an RDKit version >= 2022.09.2")
