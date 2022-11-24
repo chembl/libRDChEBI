c7681a6de14ff26c7d0d6f3256b8555669666b63
Eloy Felix <eloyfelix@gmail.com>
2022-11-24 15:43:30 +0000

    add get_mass_from_formula (avg and monoisotopic)

--- libRDChEBI/__init__.py
+++ libRDChEBI/__init__.py
@@ -8,9 +8,10 @@ from .descriptors import (
     get_inchi_and_key,
     get_monoisotopic_mass,
     get_net_charge,
+    get_mass_from_formula,
 )
 import rdkit
 
 rdkversion = rdkit.__version__.split(".")
-if rdkversion < ["2022", "09", "1"]:
+if rdkversion < ["2022", "09", "2"]:
     raise ValueError("need an RDKit version >= 2022.09.2")
