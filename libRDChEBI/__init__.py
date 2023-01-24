fdef776966c26b935a69754368ebf23dd833a010
Eloy Felix <eloyfelix@gmail.com>
2023-01-24 11:27:45 +0000

    rdkit 2022.09.4

--- libRDChEBI/__init__.py
+++ libRDChEBI/__init__.py
@@ -1,5 +1,5 @@
 import rdkit
 
 rdkversion = rdkit.__version__.split(".")
-if rdkversion < ["2022", "09", "3"]:
-    raise ValueError("need an RDKit version >= 2022.09.3")
+if rdkversion < ["2022", "09", "4"]:
+    raise ValueError("need an RDKit version >= 2022.09.4")
